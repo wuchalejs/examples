ef6a6e7bec62d62ab72c2403047dbf1d799d2a4e
K1DV5 <kidusadugna@gmail.com>
2025-08-07 20:16:25 +0200

    sveltekit-advanced: fix error on build

--- sveltekit-advanced/src/routes/[locale]/+page.ts
+++ sveltekit-advanced/src/routes/[locale]/+page.ts
@@ -2,6 +2,6 @@ import type { EntryGenerator } from './$types'
 import { locales } from 'virtual:wuchale/locales'
 
 export const entries: EntryGenerator = () => {
-	return Object.keys(locales).map(locale => ({ locale }))
+	return locales.map(locale => ({ locale }))
 }
 
