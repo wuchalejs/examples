40150520ea36a4558a8a4497f7267d66d8418880
K1DV5 <kidusadugna@gmail.com>
2025-08-17 18:47:50 +0200

    update sveltekit-advanced example for SSR

--- sveltekit-advanced/src/hooks.server.ts
+++ sveltekit-advanced/src/hooks.server.ts
@@ -1,6 +1,15 @@
 import type { Handle } from '@sveltejs/kit';
 import './locales/server/loader.js'
-import { runWithLocale } from 'wuchale/load-utils/server';
+import { runWithLocale, loadLocales } from 'wuchale/load-utils/server';
+
+// these loaders need SSR, need to load the loaders once at startup for the server
+import * as singleLoader from './locales/single/loader.svelte.js'
+import * as granularLoader from './locales/granular/loader.svelte.js'
+
+const locales = ['en', 'es']
+
+await loadLocales(singleLoader.key, singleLoader.loadIDs, singleLoader.loadCatalog, locales)
+await loadLocales(granularLoader.key, granularLoader.loadIDs, granularLoader.loadCatalog, locales)
 
 export const handle: Handle = async ({ event, resolve }) => {
     const locale = event.params.locale ?? 'en';
