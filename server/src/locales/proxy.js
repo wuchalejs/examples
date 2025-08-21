8aa925ec2f33238408f56a7f06c2ad8ae693db83
K1DV5 <kidusadugna@gmail.com>
2025-08-21 20:24:35 +0200

    server: update after new release

--- server/src/locales/proxy.js
+++ server/src/locales/proxy.js
@@ -1,8 +1,8 @@
 
-            import * as enOfmain from './en.compiled.main.js'
-import * as esOfmain from './es.compiled.main.js'
-import * as frOfmain from './fr.compiled.main.js'
-            const catalogs = {main: {en: enOfmain,es: esOfmain,fr: frOfmain}}
+            import * as _w_c_main_0_ from './en.compiled.main.js'
+import * as _w_c_main_1_ from './es.compiled.main.js'
+import * as _w_c_main_2_ from './fr.compiled.main.js'
+            const catalogs = {main: {en: _w_c_main_0_,es: _w_c_main_1_,fr: _w_c_main_2_}}
             export const loadCatalog = (loadID, locale) => catalogs[loadID][locale]
             
             export const loadIDs = ['main']
