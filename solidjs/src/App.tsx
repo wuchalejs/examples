c57d1db4c3547865e6af47dcfdd16e24feef8c5b
K1DV5 <kidusadugna@gmail.com>
2025-08-23 17:48:55 +0200

    solidjs: add loading message until catalog loads

--- solidjs/src/App.tsx
+++ solidjs/src/App.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, type Component } from 'solid-js';
+import { createEffect, createSignal, Show, type Component } from 'solid-js';
 import { loadLocale } from 'wuchale/load-utils'
 import './locales/loader.js' // so that the loader is registered
 import Counter from './Counter.jsx'
@@ -7,26 +7,30 @@ import styles from './App.module.css';
 
 const App: Component = () => {
     const [locale, setLocale] = createSignal('en')
+    const [loaded, setLoaded] = createSignal(false)
     createEffect(() => {
-        loadLocale(locale())
+        setLoaded(false)
+        loadLocale(locale()).then(() => setLoaded(true))
     })
     return (
         <div class={styles.App}>
-            <header class={styles.header}>
-                <button onClick={() => setLocale(locale() === 'en' ? 'es' : 'en')}>{locale()}</button>
-                <Counter />
-                <p>
-                    Edit 18 <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    class={styles.link}
-                    href="https://github.com/solidjs/solid"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn Solid
-                </a>
-            </header>
+            <Show when={loaded()} fallback={/* @wc-ignore */ 'Loading catalog...'}>
+                <header class={styles.header}>
+                    <button onClick={() => setLocale(locale() === 'en' ? 'es' : 'en')}>{locale()}</button>
+                    <Counter />
+                    <p>
+                        Edit 18 <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        class={styles.link}
+                        href="https://github.com/solidjs/solid"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn Solid
+                    </a>
+                </header>
+            </Show>
         </div>
     );
 };
