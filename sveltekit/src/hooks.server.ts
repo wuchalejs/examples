d7dd0d7b07a48f60a2fc5112d76836f0cfde78e8
K1DV5 <kidusadugna@gmail.com>
2025-08-29 13:25:51 +0200

    sveltekit: update after new release

--- sveltekit/src/hooks.server.ts
+++ sveltekit/src/hooks.server.ts
@@ -1,5 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
-import { loadCatalog, loadIDs, key } from './locales/loader.svelte.js'
+import { loadCatalog, loadIDs, key } from './locales/loader.ssr.svelte.js'
 import { runWithLocale, loadLocales } from 'wuchale/load-utils/server';
 import { locales } from 'virtual:wuchale/locales'
 
