ed485424cbd2e8e324b3181b13ce0ac8e3576ffa
K1DV5 <kidusadugna@gmail.com>
2025-09-01 17:52:40 +0200

    update all after new release

--- sveltekit-advanced/src/locales/es-es.compiled.server.js
+++ sveltekit-advanced/src/locales/es-es.compiled.server.js
@@ -1,13 +1,1 @@
-
-            export let c = ["¡Hola desde el servidor!","Individual","Granular","Paquete granular","Servidor","Bienvenido a SvelteKit",["Visita ",[0,"svelte.dev/docs/kit"]," para leer la documentación"],"Obtener mensaje del servidor","Esta es una subruta"]
-            let latestVersion = -1
-            export function update({ version, data }) {
-                if (latestVersion >= version) {
-                    return
-                }
-                for (const [ index, item ] of data['es-es'] ?? []) {
-                    c[index] = item
-                }
-                latestVersion = version
-            }
-        
+export let c = ["¡Hola desde el servidor!","Individual","Granular","Paquete granular","Servidor","Bienvenido a SvelteKit",["Visita ",[0,"svelte.dev/docs/kit"]," para leer la documentación"],"Obtener mensaje del servidor","Esta es una subruta"]
