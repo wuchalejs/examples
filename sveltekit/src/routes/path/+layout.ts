145093c25e59c9f0e8af6b6ca5d7f9a3f4ee0925
K1DV5 <kidusadugna@gmail.com>
2025-11-15 12:17:36 +0100

    sveltekit await parent to coordinate nested layout

--- sveltekit/src/routes/path/+layout.ts
+++ sveltekit/src/routes/path/+layout.ts
@@ -1,13 +1,7 @@
 import type { LayoutLoad } from "./$types";
-import { browser } from '$app/environment'
-import { locales } from '../../locales/data.js'
-import { loadLocale } from 'wuchale/load-utils'
 
-export const load: LayoutLoad = async ({url}) => {
-    const locale = url.searchParams.get('locale') ?? 'en'
-    if (locales.includes(locale) && browser) {
-        await loadLocale(locale)
-    }
+export const load: LayoutLoad = async ({ parent }) => {
+    await parent()
     return {
         nestedLayoutMsg: "Hello from nested layout!",
     };
