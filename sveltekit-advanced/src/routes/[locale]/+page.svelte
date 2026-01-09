df317855106eb67cac0f5fc5a5ae40460bba24d8
K1DV5 <kidusadugna@gmail.com>
2026-01-09 23:55:28 +0100

    sveltekit-advanced: add plurals example

--- sveltekit-advanced/src/routes/[locale]/+page.svelte
+++ sveltekit-advanced/src/routes/[locale]/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+
+function plural(num: number, candidates: string[], rule = (n: number) => n === 1 ? 0 : 1) {
+    const index = rule(num)
+    return candidates[index].replace('#', num.toString())
+}
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a class="text-blue-600" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>{plural(3, ['There is one item', 'There are # items (to test plurals)'])}</p>
